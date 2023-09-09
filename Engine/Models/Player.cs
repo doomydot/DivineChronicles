da5874b4d8979d0a5be14afa9d7e923e7d3d5305
doomydot <alice@somnolentgames.com>
2023-09-09 21:32:48 +0200

    MainWindow now displays playerdata

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,11 @@
+﻿namespace Engine.Models;
+
+public class Player
+{
+    public string Name { get; set; }
+    public string CharacterClass { get; set; }
+    public int HP { get; set; }
+    public int XP { get; set; }
+    public int Level { get; set; }
+    public int Gold { get; set; }
+}
---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,19 @@
+﻿using Engine.Models;
+
+namespace Engine.ViewModels;
+
+public class GameSession
+{
+    public Player CurrentPlayer { get; set; }
+    
+    public GameSession() {
+        CurrentPlayer = new Player {
+            Name = "Alice",
+            Gold = 1000000,
+            CharacterClass = "Fighter",
+            Level = 3,
+            HP = 80,
+            XP = 200
+        };
+    }
+}
