dfc663ecf38f98f363cf6e828394b162df37bb6a
doomydot <alice@somnolentgames.com>
2023-09-10 03:08:37 +0200

    GameItem, Weapon, ItemFactory

---
+++ Engine/Models/GameItem.cs
@@ -0,0 +1,18 @@
+﻿namespace Engine.Models;
+
+public class GameItem
+{
+    public int ItemTypeID { get; set; }
+    public string Name { get; set; }
+    public int Price { get; set; }
+
+    public GameItem(int itemTypeId, string name, int price) {
+        ItemTypeID = itemTypeId;
+        Name = name;
+        Price = price;
+    }
+
+    public virtual GameItem Clone() {
+        return new GameItem(ItemTypeID, Name, Price);
+    }
+}
---
+++ Engine/Models/Weapon.cs
@@ -0,0 +1,17 @@
+﻿namespace Engine.Models;
+
+public class Weapon : GameItem
+{
+    public int MinDamage { get; set; }
+    public int MaxDamage { get; set; }
+
+    public Weapon(int itemTypeID, string name, int price, int minDamage, int maxDamage) 
+        : base(itemTypeID, name, price) {
+        MinDamage = minDamage;
+        MaxDamage = maxDamage;
+    }
+    
+    public override Weapon Clone() {
+        return new Weapon(ItemTypeID, Name, Price, MinDamage, MaxDamage);
+    }
+}
